30dfd6912167f94d5c00803ab6ed8c368b04df45
jtsalva <tim@jtsalva.dev>
2019-10-26 17:31:44 +0100

    fetch all items and put into memory

--- scraping/main.go
+++ scraping/main.go
@@ -11,33 +11,83 @@ type Item struct {
 	ExpiresIn string `json:"expiresIn"`
 }
 
-const URL = "http://www.eatbydate.com/dairy/milk/soy-rice-almond-milk-substitutes-shelf-life-expiration-date//"
+const baseUrl = "http://www.eatbydate.com/"
 
 var (
-	foundItem = ""
+	lookingFor = ""
+	foundItem = false
 	opened = false
+	moveOnToNextData = false
 	items = make([]Item, 0)
 )
 
 func main() {
 	fmt.Println("Starting...")
 
+	categoryUrls := make([]string, 0)
 	collector := colly.NewCollector()
 
-	collector.OnHTML("table#unopened", handleMainDataTable)
+	collector.OnHTML("div.col-cody-12", func(e *colly.HTMLElement) {
+		e.ForEach(".heading-link", func(i int, e *colly.HTMLElement) {
+			categoryUrls = append(categoryUrls, e.Attr("href"))
+		})
+	})
 
-	err := collector.Visit(URL)
+	err := collector.Visit(baseUrl)
 	if err != nil {
 		panic(err)
 	}
 
-	for _, item := range items {
-		fmt.Printf("%s: %s\n", item.Name, item.ExpiresIn)
+	getCategories(categoryUrls)
+}
+
+func getCategories(categoryUrls []string) {
+	numberOfCategories := len(categoryUrls)
+	itemUrls := make([]string, 0)
+	collector := colly.NewCollector()
+
+	collector.OnHTML("div.fusion-content-boxes", func(e *colly.HTMLElement) {
+		e.ForEach(".heading-link", func(i int, e *colly.HTMLElement) {
+			itemUrls = append(itemUrls, e.Attr("href"))
+		})
+	})
+
+	var err error
+	for i, categoryUrl := range categoryUrls {
+		fmt.Printf("Fetching Category #%d out of %d\n", i + 1, numberOfCategories)
+
+		err = collector.Visit(categoryUrl)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	getItems(itemUrls)
+}
+
+func getItems(itemUrls []string) {
+	numberOfItems := len(itemUrls)
+	collector := colly.NewCollector()
+
+	collector.OnHTML("div.cody-green-table h2.title.cntrtxt strong", func(e *colly.HTMLElement) {
+		lookingFor = e.Text[:len(e.Text) - 16]
+	})
+	collector.OnHTML("div.fusion-column", handleMainDataTable)
+
+	var err error
+	for i, itemUrl := range itemUrls {
+		fmt.Printf("Fetching Item #%d out of %d\n", i + 1, numberOfItems)
+
+		err = collector.Visit(itemUrl)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
 	}
 }
 
 func handleMainDataTable(e *colly.HTMLElement) {
-	e.ForEach("tr", handleRow)
+	moveOnToNextData = false
+	go e.ForEach("tr", handleRow)
 }
 
 func handleRow(rowNum int, e *colly.HTMLElement) {
@@ -47,23 +97,24 @@ func handleRow(rowNum int, e *colly.HTMLElement) {
 		opened = true
 	}
 
-	e.ForEach("td", handleData)
+	if !moveOnToNextData {
+		e.ForEach("td", handleData)
+	}
 }
 
 func handleData(dataNum int, e *colly.HTMLElement) {
 	switch dataNum {
 	case 0:
-		if strings.Contains(e.Text, " lasts for") {
-			foundItem = e.Text[:len(e.Text) - 11]
-		}
+		foundItem = strings.Contains(e.Text, lookingFor)
 	case 1:
-		if foundItem != "" {
+		if foundItem {
+			fmt.Printf("\t%s: %s\n", lookingFor, e.Text)
 			items = append(items, Item{
-				Name:      foundItem,
+				Name:      lookingFor,
 				ExpiresIn: e.Text,
 			})
-		}
 
-		foundItem = ""
+			moveOnToNextData = true
+		}
 	}
 }
