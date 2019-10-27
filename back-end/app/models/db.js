a59e5cab454769bee950a2c587f2386701b2008e
jtsalva <tim@jtsalva.dev>
2019-10-27 09:49:53 +0000

    insert data into test database

--- back-end/app/models/db.js
+++ back-end/app/models/db.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://scraper:securepassword@cluster0-yvue0.gcp.mongodb.net/userinfo");
+mongoose.connect("mongodb+srv://scraper:securepassword@cluster0-yvue0.gcp.mongodb.net/test");
