69a14e42bf18485c1a45167eb51574fa85a0e335
jtsalva <tim@jtsalva.dev>
2019-10-27 10:20:32 +0000

    blah blah

--- back-end/app/routes/index.js
+++ back-end/app/routes/index.js
@@ -11,7 +11,12 @@ const chatbotController = require('../controllers/chatbot')
 const registerController = require('../controllers/registration')
 
 module.exports = function(app) {
+    app.get('/', indexController)
     app.post('/hello', chatbotController.message)
     app.post('/register', registerController.register)
-    app.get('/', indexController)
+    app.get('/thank-you', thankYouController)
+}
+
+function thankYouController(req, res) {
+    res.send("<html><head><title>Thank you!</title><style>h1 { font-size: 3em; text-align: center; }</style></head><body><br /><br /><br /><h1>Thank you " + req.query.firstName + " for registering with " + req.query.number + "</h1></body></html>")
 }
